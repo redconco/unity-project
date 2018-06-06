db7854d916443231a768715255d0c7c2ec087e6d
TaehyunGong <38848719+TaehyunGong@users.noreply.github.com>
2018-06-06 17:34:51 +0900

    Test
    원 움직이기

---
+++ Game/Assets/Camera_Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Move : MonoBehaviour {
+
+
+    public Transform target;
+    public float dist = 7f;
+    public float height = 5f;
+
+    private Transform tr;
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        tr.position = target.position - (1 * Vector3.forward * dist) + (Vector3.up * height);
+        tr.LookAt(target);
+	}
+    
+}
---
+++ Game/Assets/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move: MonoBehaviour {
+
+    public int Speed = 1;
+    public Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+
+
+    private void FixedUpdate()
+    {
+        float moverHrizontal = Input.GetAxis("Horizontal");
+        float moverVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moverHrizontal*Speed,
+            0.0f, moverVertical*Speed);
+
+        rigidbody.AddForce(movement);
+    }
+}
