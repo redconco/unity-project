2c5984b0f07a59d971cc4e42ef54529df4ceaf29
TaehyunGong <38848719+TaehyunGong@users.noreply.github.com>
2018-06-06 21:58:07 +0900

    UnityChan_import
    기여운 유니티짱

---
+++ Game/Assets/Cube_Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Move : MonoBehaviour {
+
+    public float move_sp = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(move_sp *Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-move_sp * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -move_sp * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, move_sp * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Game/Assets/UnityChan_Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChan_Move : MonoBehaviour {
+
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 360f;
+
+    CharacterController characterController;
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if(direction.sqrMagnitude > 0.01f)
+        {
+            Vector3 forward = Vector3.Slerp(transform.forward, direction, rotationSpeed * Time.deltaTime/Vector3.Angle(transform.forward, direction));
+
+            transform.LookAt(transform.position + forward);
+        }
+        //충돌을처리
+        characterController.Move(direction * moveSpeed * Time.deltaTime);
+
+        //달리는데 애니메이션 추가
+        animator.SetFloat("Speed", characterController.velocity.magnitude);
+    }
+}
